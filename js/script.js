f9d6f6bb869a7cb50a0bc45a1927e234919b9aac
ajruramadhan25 <ajruramadhan25@gmail.com>
2025-01-17 17:57:12 +0700

    update besar

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Website Loaded Successfully!");
 });
+function showModal(imagePath) {
+    // Menampilkan modal dan memuat gambar
+    var modal = document.getElementById("imageModal");
+    var modalImage = document.getElementById("modalImage");
+    modalImage.src = imagePath;
+    modal.classList.add("show");
+}
+
+function closeModal() {
+    // Menutup modal
+    var modal = document.getElementById("imageModal");
+    modal.classList.remove("show");
+}
